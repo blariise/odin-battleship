23ebd2bcc77fc00c024070a5ed972f64f7af3f11
blarrise <61951559+blariise@users.noreply.github.com>
2025-11-25 23:34:42 +0100

    Add type field to player

--- src/player.js
+++ src/player.js
@@ -2,9 +2,11 @@ import Gameboard from "./gameboard.js";
 
 export default class Player {
   gameboard;
+  type;
   
-  constructor() {
+  constructor(type) { // bot or human
     this.gameboard = new Gameboard();
     this.gameboard.populateBoard();
+    this.type = type;
   }
 }
